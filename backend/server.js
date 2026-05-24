5fe96ae3c05b80f3d060096487446dec94f828b6
machere8796 <tou01221@gmail.com>
2026-05-24 23:29:30 +0900

    add hiyoko image tool

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 
+const imageMap = require("./imageMap.json");
+
 const app = express();
 
 app.use(cors());
 
-const imageMap = {
-  1: "https://picsum.photos/id/237/500/300",
-  2: "https://picsum.photos/id/40/500/300",
-  3: "https://picsum.photos/id/1074/500/300"
-};
-
 app.get("/image/:num", (req, res) => {
-  const num = req.params.num;
 
-  res.json({
-    image:
-      imageMap[num] ||
-      "https://placehold.co/500x300?text=No+Image"
-  });
+    const num = req.params.num;
+
+    res.json({
+        image: imageMap[num] || null
+    });
+
 });
 
 app.listen(3001, () => {
-  console.log("server start");
+    console.log("server start");
 });
