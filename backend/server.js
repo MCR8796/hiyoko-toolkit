58fac0455a42dca702b64377cb3587d66a917b79
machere8796 <tou01221@gmail.com>
2026-05-25 22:30:11 +0900

    fix dependencies

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-const imageList = require("./imageMap.json");
+const imageMap = require("./imageMap.json");
 
 const app = express();
 
 app.use(cors());
 
-// 画像配信
-app.use("/images", express.static(path.join(__dirname, "image")));
+/* 追加：imageフォルダ公開 */
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "image"))
+);
 
 app.get("/", (req, res) => {
   res.send("Hiyoko API Running");
@@ -19,18 +22,18 @@ app.get("/image/:num", (req, res) => {
   const num = parseInt(req.params.num, 10);
 
   if (isNaN(num) || num < 1) {
-    return res.status(400).json({ image: null });
+    return res.json({ image: null });
   }
 
-  const fileName = imageList[num - 1];
+  const fileName = imageMap[num - 1];
 
   if (!fileName) {
-    return res.status(404).json({ image: null });
+    return res.json({ image: null });
   }
 
   res.json({
     id: num,
-    image: `/images/${fileName}`,
+    image: `/images/${encodeURIComponent(fileName)}`
   });
 });
 
